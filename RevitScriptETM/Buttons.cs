76c55b7878ba37665a4c18d46447ab0da7ad3df8
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-08-22 17:37:26 +0300

    Добавьте файлы проекта.

---
+++ RevitScriptETM/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitScriptETM
+{
+    internal class Buttons : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
