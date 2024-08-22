12094698e0da7de896af1e6ad96a38c0cd7555b5
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-08-22 17:41:43 +0300

    Добавил новую функцию Function_2

---
+++ RevitScriptETM/Function_2.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitScriptETM
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Function_2 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
