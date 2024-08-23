9f4aafa59ba77d1372868e7a9b07de17f19f2b4a
Александр Савицкий <alexandr.savitski@tut.by>
2024-08-23 15:39:56 +0300

    Добавлена панель

--- RevitScriptETM/Buttons.cs
+++ RevitScriptETM/Buttons.cs
@@ -1,14 +1,18 @@
 ﻿using Autodesk.Revit.UI;
-using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
 
 namespace RevitScriptETM
 {
     internal class Buttons : IExternalApplication
     {
+        string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+        string tabName = "ETM";
         public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
@@ -16,7 +20,40 @@ namespace RevitScriptETM
 
         public Result OnStartup(UIControlledApplication application)
         {
+            RibbonPanel panel;
+
+            if (application.GetRibbonPanels(tabName).Count > 0)
+            {
+                panel = application.GetRibbonPanels(tabName).First();
+            }
+            else application.CreateRibbonTab(tabName);
+
+            panel = application.CreateRibbonPanel(tabName, "Задания");
+
+            panel.AddItem(new PushButtonData(nameof(Tasks), "Задания", assemblyLocation, typeof(Tasks).FullName)
+            {
+                //LargeImage = GetBitmapImage(Properties.Resources.CheckArhitects),
+                LongDescription = "Выдача заданий для смежных разделов"
+            });
+
             return Result.Succeeded;
+        }
+        BitmapImage GetBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
         }
     }
 }
