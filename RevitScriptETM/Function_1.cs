d8454f7024818551786fca8287fdaba2bd06e1e4
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-08-23 16:45:11 +0300

    1321313

--- RevitScriptETM/Function_1.cs
+++ RevitScriptETM/Function_1.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace RevitScriptETM
 {
@@ -14,6 +15,9 @@ namespace RevitScriptETM
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            //MainMenu myWindow = new MainMenu();
+            MessageBox.Show("Привет","123123");
+
             return Result.Succeeded;
         }
     }
