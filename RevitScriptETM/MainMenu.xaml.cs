9293d0950e7f45fe4afee0d9786d12a237b6f0c3
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-09-09 09:42:02 +0300

    111

--- RevitScriptETM/MainMenu.xaml.cs
+++ RevitScriptETM/MainMenu.xaml.cs
@@ -48,10 +48,16 @@ namespace RevitScriptETM
         {
             if (sender is CheckBox checkbox && checkbox.DataContext is DataRowView rowView)
             {
-                rowView["TaskHandler"] = Function_1.username;
-
-                UpdateDatabaseForHandlers(rowView, Function_1.username, 1);
-                RefreshItems_CheckBox();
+                if (rowView != null)
+                {
+                    rowView["TaskHandler"] = Function_1.username;
+                    UpdateDatabaseForHandlers(rowView, Function_1.username, 1);
+                    RefreshItems_CheckBox();
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка: DataRowView не установлен.");
+                }
             }
         }
 
