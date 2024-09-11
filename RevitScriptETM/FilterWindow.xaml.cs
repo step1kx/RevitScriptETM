a0564ab5738e123160430bbcb37e501685e4d507
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-09-11 11:37:23 +0300

    Добавил файл settings, в котором прописал свойства для локального сохранения фильтра, чтобы при закрытии выбор пользователя сохранялся, а при открытии показывался

--- RevitScriptETM/FilterWindow.xaml.cs
+++ RevitScriptETM/FilterWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace RevitScriptETM
 {
@@ -28,6 +29,70 @@ namespace RevitScriptETM
         {
             InitializeComponent();
             LoadComboBoxData();
+            LoadFilterSettings();
+        }
+
+        private void SaveFilterSettings()
+        {
+            Properties.Settings.Default.FromSectionCheckBox = FromSectionCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.FromSectionTextBox = FromSectionTextBox.Text;
+
+            Properties.Settings.Default.ToSectionCheckBox = ToSectionCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.ToSectionTextBox = ToSectionTextBox.Text;
+
+            Properties.Settings.Default.TaskIssuerCheckBox = TaskIssuerCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.TaskIssuerComboBox = TaskIssuerComboBox.SelectedItem?.ToString();
+
+            Properties.Settings.Default.TaskCompletedCheckBox = TaskCompletedCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.TaskCompletedComboBox = TaskCompletedComboBox.SelectedItem?.ToString();
+
+            Properties.Settings.Default.TaskApprovalCheckBox = TaskApprovalCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.TaskApprovalComboBox = TaskApprovalComboBox.SelectedItem?.ToString();
+
+            Properties.Settings.Default.WhoApprovalCheckBox = WhoApprovalCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.WhoApprovalComboBox = WhoApprovalComboBox.SelectedItem?.ToString();
+
+            Properties.Settings.Default.TaskHandlerCheckBox = TaskHandlerCheckBox.IsChecked ?? false;
+            Properties.Settings.Default.TaskHandlerComboBox = TaskHandlerComboBox.SelectedItem?.ToString();
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadFilterSettings()
+        {
+            // Проверяем наличие значения перед установкой
+            FromSectionCheckBox.IsChecked = Properties.Settings.Default.FromSectionCheckBox;
+            FromSectionTextBox.Text = Properties.Settings.Default.FromSectionTextBox;
+
+            ToSectionCheckBox.IsChecked = Properties.Settings.Default.ToSectionCheckBox;
+            ToSectionTextBox.Text = Properties.Settings.Default.ToSectionTextBox;
+
+            TaskIssuerCheckBox.IsChecked = Properties.Settings.Default.TaskIssuerCheckBox;
+            TaskIssuerComboBox.SelectedItem = Properties.Settings.Default.TaskIssuerComboBox;
+
+            TaskCompletedCheckBox.IsChecked = Properties.Settings.Default.TaskCompletedCheckBox;
+            if (Properties.Settings.Default.TaskCompletedComboBox != null)
+            {
+                TaskCompletedComboBox.SelectedItem = Properties.Settings.Default.TaskCompletedComboBox;
+            }
+
+            TaskApprovalCheckBox.IsChecked = Properties.Settings.Default.TaskApprovalCheckBox;
+            if (Properties.Settings.Default.TaskApprovalComboBox != null)
+            {
+                TaskApprovalComboBox.SelectedItem = Properties.Settings.Default.TaskApprovalComboBox;
+            }
+
+            WhoApprovalCheckBox.IsChecked = Properties.Settings.Default.WhoApprovalCheckBox;
+            if (Properties.Settings.Default.WhoApprovalComboBox != null)
+            {
+                WhoApprovalComboBox.SelectedItem = Properties.Settings.Default.WhoApprovalComboBox;
+            }
+
+            TaskHandlerCheckBox.IsChecked = Properties.Settings.Default.TaskHandlerCheckBox;
+            if (Properties.Settings.Default.TaskHandlerComboBox != null)
+            {
+                TaskHandlerComboBox.SelectedItem = Properties.Settings.Default.TaskHandlerComboBox;
+            }
         }
 
         private void LoadComboBoxData()
@@ -107,53 +172,44 @@ namespace RevitScriptETM
 
         private void ApplyButton_ClickFilter(object sender, RoutedEventArgs e)
         {
+            // Сохранение состояния фильтров
+            
+
+            // Построение запроса с учетом фильтров
             string query = "SELECT * FROM [Table] WHERE 1=1";
 
             if (FromSectionCheckBox.IsChecked == true && !string.IsNullOrEmpty(FromSectionTextBox.Text))
             {
                 query += $" AND FromSection = N'{FromSectionTextBox.Text}'";
             }
-            // Фильтр по ToSection
             if (ToSectionCheckBox.IsChecked == true && !string.IsNullOrEmpty(ToSectionTextBox.Text))
             {
                 query += $" AND ToSection = N'{ToSectionTextBox.Text}'";
             }
-
-            // Фильтр по TaskIssuer
             if (TaskIssuerCheckBox.IsChecked == true && TaskIssuerComboBox.SelectedItem != null)
             {
                 query += $" AND TaskIssuer = N'{TaskIssuerComboBox.SelectedItem}'";
             }
-
-            // Фильтр по TaskHandler
             if (TaskHandlerCheckBox.IsChecked == true && TaskHandlerComboBox.SelectedItem != null)
             {
                 query += $" AND TaskHandler = N'{TaskHandlerComboBox.SelectedItem}'";
             }
-
-            // Фильтр по TaskCompleted
             if (TaskCompletedCheckBox.IsChecked == true)
             {
                 string taskCompletedValue = (TaskCompletedComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
                 int completed = taskCompletedValue == "Выполнил" ? 1 : 0;
                 query += $" AND TaskCompleted = {completed}";
             }
-
-            // Фильтр по TaskApproval
             if (TaskApprovalCheckBox.IsChecked == true)
             {
                 string taskApprovalValue = (TaskApprovalComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
                 int approved = taskApprovalValue == "Согласовал" ? 1 : 0;
                 query += $" AND TaskApproval = {approved}";
             }
-
-            // Фильтр по WhoApproval
             if (WhoApprovalCheckBox.IsChecked == true && WhoApprovalComboBox.SelectedItem != null)
             {
                 query += $" AND WhoApproval = '{WhoApprovalComboBox.SelectedItem}'";
             }
-
-            // ... добавьте другие фильтры, если необходимо ...
 
             string connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename={Function_1.documentDirectory}\Tasks.mdf; Integrated Security=True";
 
@@ -166,20 +222,24 @@ namespace RevitScriptETM
                     DataTable resultTable = new DataTable();
                     dataAdapter.Fill(resultTable);
 
+                    SaveFilterSettings();
+
                     // Вызовите событие с отфильтрованными данными
                     FilterDone?.Invoke(this, resultTable);
                     DialogResult = true;
+
                     Close();
                 }
-                
             }
         }
+
+
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = false;
             Close();
         }
-    
+
     }
 }
