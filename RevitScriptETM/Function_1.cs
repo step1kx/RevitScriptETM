a5f16f269219eae6fcc724021a2526a6c6ad9945
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-09-20 17:38:32 +0300

    бд

--- RevitScriptETM/Function_1.cs
+++ RevitScriptETM/Function_1.cs
@@ -16,8 +16,7 @@ namespace RevitScriptETM
     {     
         public static string username;
         public static List<View> views;
-        public static string documentDirectory = @"\\192.168.53.190\bim\01-Объекты\DataBaseTasks\";
-        public static string dbName;
+       
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -30,28 +29,15 @@ namespace RevitScriptETM
             views = collector.OfClass(typeof(View)).Cast<View>().ToList();
 
             string BDTamplatePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            dbName = DBName(doc);
 
-            try
-            {
-                if (!File.Exists(documentDirectory + dbName))
-                {
-                    File.Copy(Path.Combine(BDTamplatePath, "Tasks.mdf"), Path.Combine(documentDirectory, $"{dbName}.mdf"), false);
-                    File.Copy(Path.Combine(BDTamplatePath, "Tasks_log.ldf"), Path.Combine(documentDirectory, $"{dbName}_log.ldf"), false);
-                }
-            }
-            catch (System.Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
 
-            SqlConnection conn = new SqlConnection($@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = {documentDirectory}\{DBName(doc)}.mdf; Integrated Security = True", null);
+
             MainMenu myWindow = new MainMenu();
 
-            using (conn)
+            using (dbSqlConnection.conn)
             {
-                conn.Open();
-                SqlCommand createCommand = new SqlCommand("SELECT * FROM [Table]", conn);
+                dbSqlConnection.conn.Open();
+                SqlCommand createCommand = new SqlCommand("SELECT * FROM [Table]", dbSqlConnection.conn);
                 createCommand.ExecuteNonQuery();
                 SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
                 DataTable dt = new DataTable("Table"); // В скобках указываем название таблицы
@@ -59,20 +45,12 @@ namespace RevitScriptETM
                 myWindow.tasksDataGrid.ItemsSource = dt.DefaultView; // Сам вывод 
 
             }
-            conn.Close();
+            dbSqlConnection.conn.Close();
             myWindow.ShowDialog();
             myWindow.RefreshItems();
 
             return Result.Succeeded; 
         }
-        string DBName(Document doc)
-        {
-            string pathName = doc.PathName;
-            int lastIndexSlash = pathName.LastIndexOf('\\');
-            int lastIndexDot = pathName.LastIndexOf(".");
-            string projectName = pathName.Substring(lastIndexSlash + 1, lastIndexDot - lastIndexSlash - 1);
-            string dbName = projectName.Substring(0, projectName.IndexOf("-"));
-            return dbName;
-        }
+        
     }
 }
--- RevitScriptETM/dbSqlConnection.cs
+++ RevitScriptETM/dbSqlConnection.cs
@@ -5,7 +5,10 @@ namespace RevitScriptETM
 {
     internal class dbSqlConnection
     {
-        SqlConnection conn = new SqlConnection($@"Data Source =  Integrated Security = True", null);
+        public static SqlConnection conn = new SqlConnection($"Host=192.168.0.159;Port=5432;" +
+            $"Username=User;" +
+            $"Password=123;" +
+            $"Database=Table;");
 
     }
 }
