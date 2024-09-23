bc097764c7e12ef94c0408401c49b23ca2c77c15
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-09-23 11:15:25 +0300

    111

--- RevitScriptETM/Function_2.cs
+++ RevitScriptETM/Function_2.cs
@@ -21,13 +21,9 @@ namespace RevitScriptETM
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Autodesk.Revit.DB.Document doc = uidoc.Document;
 
-            string pathName = doc.PathName;
-            int lastIndexSlash = pathName.LastIndexOf('\\');
-            int lastIndexDot = pathName.LastIndexOf(".");
-            string projectName = pathName.Substring(lastIndexSlash+1, lastIndexDot - lastIndexSlash-1);
-            string dbName = projectName.Substring(0, projectName.IndexOf("-"));
+            
 
-            MessageBox.Show(dbName);
+            
 
             return Result.Succeeded; 
         }
